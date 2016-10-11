14f890b3995da17ae16c55d8dd59b1dd54cd6ccb
Chiedu99 <chiedu.nduka-eze@ssfs.org>
2016-10-11 14:05:45 -0400

    better loops

--- loops.py
+++ loops.py
@@ -1,7 +1,7 @@
 #Chiedu Nduka-Eze Unit 5 10/10/16
 #This program prompts the user for a guess between 1-100
-
 import random
+
 
 def askUser():
     """This function asks the user if they want to play the game"""
@@ -12,10 +12,12 @@ def askUser():
         elif answer == "no":
             return False
 
+
 def randomNumber():
     """This function generates a random number"""
-    number = random.randint(1,100)
+    number = random.randint(1, 100)
     return number
+
 
 def userGuess():
     """This function makes sure the guess is between 1 and 100"""
@@ -24,7 +26,8 @@ def userGuess():
         if guess >= 1 and guess <= 100:
             return guess
 
-def tellUser(guess,number):
+
+def tellUser(guess, number):
     """This function tells the user if their number is too small or too big. It also tells them if they are correct"""
     if guess > number:
         print("you are incorrect, the number is lower!")
@@ -37,19 +40,15 @@ def tellUser(guess,number):
         return False
 
 
-
-
 def main():
     while askUser():
         numberOfGuesses = 1
         number = randomNumber()
         guess = userGuess()
-        while tellUser(guess,number):
+        while tellUser(guess, number):
             numberOfGuesses += 1
             guess = userGuess()
-        print("you tried",numberOfGuesses, "time(s)")
-
-
+        print("you tried", numberOfGuesses, "time(s)")
 
 
 main()
